1c66398fce021b9d88110e6c6675ebd73bd294c3
hanzy48633 <hanzlakhalid143@gmail.com>
2021-05-23 01:43:10 +0500

    initial commit

---
+++ ecom/itemspage/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+CATEGORY_CHOICES = (
+    ('FR', 'Fruit'),
+    ('VG', 'Vegetable'),
+    ('GR', 'Grocery Item'),
+    ('MT', 'Meat'),
+)
+
+PRICE_CATEGORY = (
+    ('PP', 'per piece'),
+    ('PK', 'per Kg')
+)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    price_type = models.CharField(choices=PRICE_CATEGORY, max_length=2, default='PP')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    quantity = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
