91cfcf081c68e3ca92483992de05cce2e105a6f8
hanzy48633 <hanzlakhalid143@gmail.com>
2021-05-25 16:08:55 +0500

    added single item product details page

--- ecom/itemspage/urls.py
+++ ecom/itemspage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, productDetail
 
 
 urlpatterns = [
-    path('', index, name='index-view')
+    path('', index, name='index-view'),
+    path('<int:_id>/', productDetail, name='product-view')
 ]
--- ecom/itemspage/views.py
+++ ecom/itemspage/views.py
@@ -7,3 +7,11 @@ def index(request):
         'items': Item.objects.all()
     }
     return render(request, 'itemspage/mainpage.html', context)
+
+
+def productDetail(request, _id):
+    context = {
+        'item': Item.objects.get(id=_id)
+    }
+
+    return render(request, 'itemspage/productpage.html', context)
